5aff010788e41e95997c7c5bd2ce6de731a56cd8
Jinyoung <jinyoung.eom@gmail.com>
2016-09-01 10:10:25 +0900

    .

---
+++ webapp01/src/main/java/example/servlet/LoginServlet.java
@@ -0,0 +1,40 @@
+package example.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws ServletException, IOException {
+    response.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<title>로그인</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>로그인</h1>");
+    out.println("<form action='login' method='post'>");
+    out.println("이메일: <input type='text' name='email'><br>");
+    out.println("암호: <input type='password' name='password'><br>");
+    out.println("<button>로그인</button>");
+    out.println("</form>");
+    out.println("</body>");
+    out.println("</html>");
+  }
+}
+
+
+
+
+
+
+
