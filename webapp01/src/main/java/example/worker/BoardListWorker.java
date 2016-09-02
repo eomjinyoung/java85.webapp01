f90ac320053f0470fcdb07427bcfb292930a004e
Jinyoung <jinyoung.eom@gmail.com>
2016-09-02 11:25:37 +0900

    .

--- webapp01/src/main/java/example/worker/BoardListWorker.java
+++ webapp01/src/main/java/example/worker/BoardListWorker.java
@@ -1,6 +1,5 @@
 package example.worker;
 
-import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.List;
 
@@ -21,58 +20,33 @@ public class BoardListWorker implements Worker {
   
   @Override
   public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-    int pageNo = 1;
-    int length = 5;
-    
-    if (request.getParameter("pageNo") != null) {
-      pageNo = Integer.parseInt(request.getParameter("pageNo"));
-    }
-    
-    if (request.getParameter("length") != null) {
-      length = Integer.parseInt(request.getParameter("length"));
-    }
-    
-    HashMap<String,Object> map = new HashMap<>();
-    map.put("startIndex", (pageNo - 1) * length);
-    map.put("length", length);
-    
-    response.setContentType("text/html;charset=UTF-8");
-    PrintWriter out = response.getWriter();
-    
-    out.println("<html>");
-    out.println("<head>");
-    out.println("  <title>게시물 목록조회</title>");
-    out.println("</head>");
-    out.println("<body>");
-    
-    // 페이지 상단에 정보 출력하기 위해 include 기법 사용.
-    RequestDispatcher rd = request.getRequestDispatcher("/header");
-    rd.include(request, response); // 실행하고 돌아와야 하기 때문에 include 여야 한다.
-    
-    out.println("<h1>게시물 목록조회</h1>");
-    out.println("<p><a href='form.html'>새 글</a></p>");
-    
     try {
-      List<Board> list = boardDao.selectList(map);
-      for (Board b : list) {
-        out.printf("%d, <a href='detail.do?no=%1$d'>%s</a>, %s, %s, %d<br>\n", 
-            b.getNo(), b.getTitle(), b.getWriter(), b.getCreatedDate(), b.getViewCount());
+      int pageNo = 1;
+      int length = 5;
+      
+      if (request.getParameter("pageNo") != null) {
+        pageNo = Integer.parseInt(request.getParameter("pageNo"));
       }
       
-    } catch (Exception e) {
-      //ServletRequest 보관소에 오류 정보 저장
-      request.setAttribute("error", e);
+      if (request.getParameter("length") != null) {
+        length = Integer.parseInt(request.getParameter("length"));
+      }
       
-      rd = request.getRequestDispatcher("/error");
+      HashMap<String,Object> map = new HashMap<>();
+      map.put("startIndex", (pageNo - 1) * length);
+      map.put("length", length);
+    
+      List<Board> list = boardDao.selectList(map);
+      request.setAttribute("list", list);
+      
+      RequestDispatcher rd = request.getRequestDispatcher("/board/BoardList.jsp");
+      rd.forward(request, response);
+      
+    } catch (Exception e) {
+      request.setAttribute("error", e);
+      RequestDispatcher rd = request.getRequestDispatcher("/error");
       rd.forward(request, response);
     }
-    
-    // 페이지 하단에 정보 출력하기 위해 include 기법 사용.
-    rd = request.getRequestDispatcher("/footer");
-    rd.include(request, response); // 실행하고 돌아와야 하기 때문에 include 여야 한다.
-    
-    out.println("</body>");
-    out.println("</html>");
   }
 
 }
---
+++ webapp01/src/main/webapp/board/BoardList.jsp
@@ -0,0 +1,35 @@
+<%@page import="example.vo.Board"%>
+<%@page import="java.util.List"%>
+<%@ page language="java" 
+         contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8"%>      
+<%
+String email = (String)request.getAttribute("email");
+String checked = (String)request.getAttribute("checked");
+%>  
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>게시물 목록조회</title>
+</head>
+<body>
+<jsp:include page="/header"></jsp:include>
+<h1>게시물 목록조회2</h1>
+<p><a href='form.html'>새 글</a></p>
+<%
+List<Board> list = (List<Board>)request.getAttribute("list");
+for (Board board : list) {%>
+<%=board.getNo()%>, <a href='detail.do?no=<%=board.getNo()%>'><%=board.getTitle()%></a>, <%=board.getWriter()%>, <%=board.getCreatedDate()%>, <%=board.getViewCount()%><br>  
+<%}%>
+<jsp:include page="/footer"></jsp:include>
+</body>
+</html>
+
+
+
+
+
+
+
+
