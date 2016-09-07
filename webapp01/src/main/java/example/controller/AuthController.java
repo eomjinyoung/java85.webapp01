d210bd56221e21d4368124a309f8e27a08645c4c
Jinyoung <jinyoung.eom@gmail.com>
2016-09-07 15:46:51 +0900

    .

--- webapp01/src/main/java/example/controller/AuthController.java
+++ webapp01/src/main/java/example/controller/AuthController.java
@@ -3,85 +3,71 @@ package example.controller;
 import java.util.HashMap;
 
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import example.dao.MemberDao;
 import example.vo.Member;
 
-//@Component
-@Controller // 스프링에서 페이지 컨트롤러에게 붙이라고 만들어준 애노테이션이다.
-@RequestMapping("/auth/") // 이 페이지 컨트롤러의 기본 URL을 지정한다.
+@Controller 
+@RequestMapping("/auth/") 
+@SessionAttributes({"member"}) // Model 객체에 저장된 정보 중에서 로그인 회원 정보는 따로 세션으로 관리한다.
 public class AuthController {
   
   @Autowired MemberDao memberDao;
   
   @RequestMapping("form")
-  public String form(HttpServletRequest request) throws Exception {
-    Cookie[] cookies = request.getCookies();
-    String email = "";
-    String checked = "";
-    if (cookies != null) {
-      for (Cookie cookie : cookies) {
-        if (cookie.getName().equals("email")) {
-          email = cookie.getValue();
-          checked = "checked";
-          break;
-        }
-      }
-    }
-    
-    request.setAttribute("email", email);
-    request.setAttribute("checked", checked);
-    
+  public String form(
+      @CookieValue(name="email", defaultValue="") String email,
+      Model model) throws Exception {
+    model.addAttribute("email", email);
+    model.addAttribute("checked", ((email.equals("")) ? "" : "checked"));
     return "/auth/LoginForm.jsp";
   }
   
   @RequestMapping("login")
   public String login(
       HttpServletResponse response,
-      HttpSession session,
-      @RequestParam(name="email") String email,
-      @RequestParam(name="password") String password,
-      @RequestParam(name="saveEmail", defaultValue="off") String saveEmail) throws Exception {
+      String email,
+      String password,
+      String saveEmail, 
+      Model model,
+      SessionStatus sessionStatus) throws Exception {
     
     Cookie cookie = new Cookie("email", email);
-    if (saveEmail.equals("off")) {
-      cookie.setMaxAge(0); // 유효기간이 0이면 웹브라우저는 "email" 이름으로 저장된 쿠키를 삭제한다.
+    if (saveEmail == null) {
+      cookie.setMaxAge(0); 
     } else {
-      cookie.setMaxAge(60 * 60 * 24 * 7); // 쿠키를 1주일 저장한다.
+      cookie.setMaxAge(60 * 60 * 24 * 7);
     }
     response.addCookie(cookie);
     
     
-    // DB에서 해당 이메일과 암호가 일치하는 사용자가 있는지 조사한다.
     HashMap<String,Object> paramMap = new HashMap<>();
     paramMap.put("email", email);
     paramMap.put("password", password);
-    
     Member member = memberDao.selectOneByEmailAndPassword(paramMap);
     
     if (member == null) {
-      // 로그인 실패한다면, 기존 세션도 무효화시킨다.
-      session.invalidate();
+      sessionStatus.setComplete(); // 스프링이 관리하는 세션 값을 무효화시킨다.
       return "/auth/LoginFail.jsp";
       
     } else {
-      // 로그인 성공 했다면, 다른 서블릿이 로그인한 사용자 정보를 사용할 수 있도록 세션에 보관한다.
-      session.setAttribute("member", member);
+      model.addAttribute("member", member); // Model 객체에 로그인 회원 정보를 담는다.
       return "redirect:../board/list.do";
     }
   }
   
   @RequestMapping("logout")
-  public String logout(HttpSession session) throws Exception {
-    session.invalidate();
+  public String logout(SessionStatus sessionStatus) throws Exception {
+    sessionStatus.setComplete();
     return "redirect:form.do";
   }
 }
--- webapp01/src/main/java/example/controller/BoardController.java
+++ webapp01/src/main/java/example/controller/BoardController.java
@@ -3,76 +3,52 @@ package example.controller;
 import java.util.HashMap;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import example.dao.BoardDao;
 import example.vo.Board;
 
-//@Component
-@Controller // 페이지 컨트롤러에 붙이는 애노테이션 
-@RequestMapping("/board/") // 이 페이지 컨트롤러의 기본 URL을 지정한다.
+@Controller 
+@RequestMapping("/board/")
 public class BoardController {
+  
   @Autowired BoardDao boardDao;
   
   @RequestMapping("list")
   public String list(
-      HttpServletRequest request,
-      @RequestParam(name="pageNo", defaultValue="1") int pageNo,
-      @RequestParam(name="length", defaultValue="5") int length) throws Exception {
+      @RequestParam(defaultValue="1") int pageNo,
+      @RequestParam(defaultValue="5") int length,
+      Model model) throws Exception {
 
     HashMap<String,Object> map = new HashMap<>();
     map.put("startIndex", (pageNo - 1) * length);
     map.put("length", length);
   
     List<Board> list = boardDao.selectList(map);
-    request.setAttribute("list", list);
+    model.addAttribute("list", list);
     
     return "/board/BoardList.jsp";
   }
   
   @RequestMapping("add")
-  public String add(
-      @RequestParam(name="password") String password,
-      @RequestParam(name="title") String title,
-      @RequestParam(name="contents") String contents) throws Exception {
-    Board board = new Board();
-    board.setPassword(password);
-    board.setTitle(title);
-    board.setContents(contents);
-    
+  public String add(Board board) throws Exception {
     boardDao.insert(board);
-    
     return "redirect:list.do";
   }
   
   @RequestMapping("detail")
-  public String detail(
-      HttpServletRequest request,
-      @RequestParam(name="no") int no) throws Exception {
+  public String detail(int no, Model model) throws Exception {
     Board board = boardDao.selectOne(no);
-    request.setAttribute("board", board);
-    
+    model.addAttribute("board", board);
     return "/board/BoardDetail.jsp";
   }
   
   @RequestMapping("update")
-  public String update(
-      @RequestParam(name="no") int no,
-      @RequestParam(name="password") String password,
-      @RequestParam(name="title") String title,
-      @RequestParam(name="contents") String contents) throws Exception {
-    
-    Board board = new Board();
-    board.setNo(no);
-    board.setTitle(title);
-    board.setContents(contents);
-    board.setPassword(password);
-    
+  public String update(Board board) throws Exception {
     HashMap<String,Object> paramMap = new HashMap<>();
     paramMap.put("no", board.getNo());
     paramMap.put("password", board.getPassword());
@@ -80,17 +56,12 @@ public class BoardController {
     if (boardDao.selectOneByPassword(paramMap) == null) {
       throw new Exception("해당 게시물이 없거나 암호가 일치하지 않습니다!");
     }
-
     boardDao.update(board);
-    
     return "redirect:list.do";
   }
   
   @RequestMapping("delete")
-  public String delete(
-      HttpServletRequest request,
-      @RequestParam(name="no") int no) throws Exception {
-    
+  public String delete(int no) throws Exception {
     boardDao.delete(no);
     return "redirect:list.do";
   }
