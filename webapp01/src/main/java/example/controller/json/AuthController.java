98e49c85fed1e93552eac875e821d756ebae4cfd
Jinyoung <jinyoung.eom@gmail.com>
2016-09-28 11:01:17 +0900

    .

--- webapp01/src/main/java/example/controller/json/AuthController.java
+++ webapp01/src/main/java/example/controller/json/AuthController.java
@@ -4,14 +4,18 @@ import java.util.HashMap;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+
+import com.google.gson.Gson;
 
 import example.dao.MemberDao;
 import example.vo.Member;
@@ -23,16 +27,8 @@ public class AuthController {
   
   @Autowired MemberDao memberDao;
   
-  @RequestMapping("form")
-  public String form(
-      @CookieValue(name="email", defaultValue="") String email,
-      Model model) throws Exception {
-    model.addAttribute("email", email);
-    model.addAttribute("checked", ((email.equals("")) ? "" : "checked"));
-    return "/auth/LoginForm.jsp";
-  }
-  
-  @RequestMapping("login")
+  @RequestMapping(path="login", produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+  @ResponseBody
   public String login(
       HttpServletResponse response,
       String email,
@@ -41,34 +37,70 @@ public class AuthController {
       Model model,
       SessionStatus sessionStatus) throws Exception {
     
-    Cookie cookie = new Cookie("email", email);
-    if (saveEmail == null) {
-      cookie.setMaxAge(0); 
-    } else {
-      cookie.setMaxAge(60 * 60 * 24 * 7);
-    }
-    response.addCookie(cookie);
-    
-    
-    HashMap<String,Object> paramMap = new HashMap<>();
-    paramMap.put("email", email);
-    paramMap.put("password", password);
-    Member member = memberDao.selectOneByEmailAndPassword(paramMap);
-    
-    if (member == null) {
-      sessionStatus.setComplete(); // 스프링이 관리하는 세션 값을 무효화시킨다.
-      return "/auth/LoginFail.jsp";
+    HashMap<String,Object> result = new HashMap<>();
+    try {
+      Cookie cookie = new Cookie("email", email);
+      if (saveEmail == null) {
+        cookie.setMaxAge(0); 
+      } else {
+        cookie.setMaxAge(60 * 60 * 24 * 7);
+      }
+      response.addCookie(cookie);
       
-    } else {
-      model.addAttribute("member", member); // Model 객체에 로그인 회원 정보를 담는다.
-      return "redirect:../board/list.do";
+      
+      HashMap<String,Object> paramMap = new HashMap<>();
+      paramMap.put("email", email);
+      paramMap.put("password", password);
+      Member member = memberDao.selectOneByEmailAndPassword(paramMap);
+      
+      if (member == null) {
+        sessionStatus.setComplete(); // 스프링이 관리하는 세션 값을 무효화시킨다.
+        result.put("state", "fail");
+        
+      } else {
+        model.addAttribute("member", member); // Model 객체에 로그인 회원 정보를 담는다.
+        result.put("state", "success");
+      }
+      
+    } catch (Exception e) {
+      result.put("state", "error");
+      result.put("data", e.getMessage());
     }
+    return new Gson().toJson(result);  
   }
   
-  @RequestMapping("logout")
-  public String logout(SessionStatus sessionStatus) throws Exception {
-    sessionStatus.setComplete();
-    return "redirect:form.do";
+  @RequestMapping(path="logout", produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+  @ResponseBody
+  public String logout(HttpSession session, SessionStatus sessionStatus) throws Exception {
+    HashMap<String,Object> result = new HashMap<>();
+    try {
+      sessionStatus.setComplete();
+      session.invalidate();
+      result.put("state", "success");
+    } catch (Exception e) {
+      result.put("state", "error");
+      result.put("data", e.getMessage());
+    }
+    return new Gson().toJson(result);   
+  }
+  
+  @RequestMapping(path="loginUser", produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+  @ResponseBody
+  public String loginUser(HttpSession session) throws Exception {
+    
+    HashMap<String,Object> result = new HashMap<>();
+    try {
+      Member member = (Member)session.getAttribute("member");
+      if (member == null) {
+        throw new Exception("로그인이 되지 않았습니다.");
+      }
+      result.put("state", "success");
+      result.put("data", member);
+    } catch (Exception e) {
+      result.put("state", "error");
+      result.put("data", e.getMessage());
+    }
+    return new Gson().toJson(result);  
   }
 }
 
