3492618e30d05c2d959a95708d41c95e0e620e8b
Jinyoung <jinyoung.eom@gmail.com>
2016-10-13 17:51:20 +0900

    .

--- webapp01/src/main/java/example/controller/BoardController.java
+++ webapp01/src/main/java/example/controller/BoardController.java
@@ -1,7 +1,5 @@
 package example.controller;
 
-import java.io.File;
-import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.ServletContext;
@@ -13,32 +11,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import example.dao.BoardDao;
-import example.dao.BoardFileDao;
-import example.util.FileUploadUtil;
+import example.service.BoardService;
 import example.vo.Board;
-import example.vo.BoardFile;
 
 @Controller 
 @RequestMapping("/board/")
 public class BoardController {
   @Autowired ServletContext sc;
-  @Autowired BoardDao boardDao;
-  @Autowired BoardFileDao boardFileDao;
+  @Autowired BoardService boardService;
   
   @RequestMapping("list")
   public String list(
       @RequestParam(defaultValue="1") int pageNo,
       @RequestParam(defaultValue="5") int length,
       Model model) throws Exception {
-
-    HashMap<String,Object> map = new HashMap<>();
-    map.put("startIndex", (pageNo - 1) * length);
-    map.put("length", length);
-  
-    List<Board> list = boardDao.selectList(map);
+    List<Board> list = boardService.getBoardList(pageNo, length);
     model.addAttribute("list", list);
-    
     return "board/BoardList";
   }
   
@@ -47,63 +35,27 @@ public class BoardController {
       Board board,
       MultipartFile file1,
       MultipartFile file2) throws Exception {
-    
-    boardDao.insert(board);
-    
-    String newFilename = null;
-    if (!file1.isEmpty()) {
-      newFilename = FileUploadUtil.getNewFilename(file1.getOriginalFilename());
-      try {
-        file1.transferTo(new File(sc.getRealPath("/upload/" + newFilename)));
-        BoardFile boardFile = new BoardFile();
-        boardFile.setFilename(newFilename);
-        boardFile.setBoardNo(board.getNo());
-        boardFileDao.insert(boardFile);
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-    }
-    
-    if (!file2.isEmpty()) {
-      newFilename = FileUploadUtil.getNewFilename(file2.getOriginalFilename());
-      try {
-        file2.transferTo(new File(sc.getRealPath("/upload/" + newFilename)));
-        BoardFile boardFile = new BoardFile();
-        boardFile.setFilename(newFilename);
-        boardFile.setBoardNo(board.getNo());
-        boardFileDao.insert(boardFile);
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-    }
-    
-    
+    String uploadDir = sc.getRealPath("/upload") + "/";
+    boardService.insertBoard(board, file1, file2, uploadDir);
     return "redirect:list.do";
   }
   
   @RequestMapping("detail")
   public String detail(int no, Model model) throws Exception {
-    Board board = boardDao.selectOne(no);
+    Board board = boardService.getBoard(no);
     model.addAttribute("board", board);
     return "board/BoardDetail";
   }
   
   @RequestMapping("update")
   public String update(Board board) throws Exception {
-    HashMap<String,Object> paramMap = new HashMap<>();
-    paramMap.put("no", board.getNo());
-    paramMap.put("password", board.getPassword());
-    
-    if (boardDao.selectOneByPassword(paramMap) == null) {
-      throw new Exception("해당 게시물이 없거나 암호가 일치하지 않습니다!");
-    }
-    boardDao.update(board);
+    boardService.updateBoard(board);
     return "redirect:list.do";
   }
   
   @RequestMapping("delete")
   public String delete(int no) throws Exception {
-    boardDao.delete(no);
+    boardService.deleteBoard(no);
     return "redirect:list.do";
   }
 }
---
+++ webapp01/src/main/java/example/service/BoardService.java
@@ -0,0 +1,90 @@
+package example.service;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import example.dao.BoardDao;
+import example.dao.BoardFileDao;
+import example.util.FileUploadUtil;
+import example.vo.Board;
+import example.vo.BoardFile;
+
+@Service 
+public class BoardService {
+  @Autowired BoardDao boardDao;
+  @Autowired BoardFileDao boardFileDao;
+  
+  public List<Board> getBoardList(int pageNo, int length) throws Exception {
+    HashMap<String,Object> map = new HashMap<>();
+    map.put("startIndex", (pageNo - 1) * length);
+    map.put("length", length);
+    return boardDao.selectList(map);
+  }
+  
+  public void insertBoard(Board board, 
+      MultipartFile file1,
+      MultipartFile file2,
+      String uploadDir) throws Exception {
+    
+    boardDao.insert(board);
+    
+    String newFilename = null;
+    if (!file1.isEmpty()) {
+      newFilename = FileUploadUtil.getNewFilename(file1.getOriginalFilename());
+      try {
+        file1.transferTo(new File(uploadDir + newFilename));
+        BoardFile boardFile = new BoardFile();
+        boardFile.setFilename(newFilename);
+        //boardFile.setBoardNo(board.getNo());
+        boardFile.setBoardNo(10200);
+        boardFileDao.insert(boardFile);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    
+    if (!file2.isEmpty()) {
+      newFilename = FileUploadUtil.getNewFilename(file2.getOriginalFilename());
+      try {
+        file2.transferTo(new File(uploadDir + newFilename));
+        BoardFile boardFile = new BoardFile();
+        boardFile.setFilename(newFilename);
+        boardFile.setBoardNo(board.getNo());
+        boardFileDao.insert(boardFile);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
+  
+  public Board getBoard(int no) throws Exception {
+    return boardDao.selectOne(no);
+  }
+  
+  public void updateBoard(Board board) throws Exception {
+    HashMap<String,Object> paramMap = new HashMap<>();
+    paramMap.put("no", board.getNo());
+    paramMap.put("password", board.getPassword());
+    
+    if (boardDao.selectOneByPassword(paramMap) == null) {
+      throw new Exception("해당 게시물이 없거나 암호가 일치하지 않습니다!");
+    }
+    boardDao.update(board);
+  }
+  
+  public void deleteBoard(int no) throws Exception {
+    boardDao.delete(no);
+  }
+}
+
+
+
+
+
+
+
